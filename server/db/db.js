b2c8f1b8f34af4b725db1fa440b4b057f7043b95
Chris <christophertberg@gmail.com>
2020-10-21 16:59:30 -0700

    prepared for migration from mLab to Atlas

--- server/db/db.js
+++ server/db/db.js
@@ -3,25 +3,25 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 if (process.env.NODE_ENV !== 'production') {
-  const mongoURI = process.env.MONGODB_URI;
+    const mongoURI = process.env.MONGODB_URI;
 }
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI || mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    try {
+        await mongoose.connect(process.env.DB_URI || mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
 
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.error(err.message);
+        console.log('MongoDB Connected');
+    } catch (err) {
+        console.error(err.message);
 
-    //Exit process with failure.
-    process.exit(1);
-  }
+        //Exit process with failure.
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
